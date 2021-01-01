c9b22c6d9df5e27fc19ea51a268406b0ee637cec
Irie Aoi <eai@mizle.net>
2021-01-01 13:38:13 +0900

    分割代入するの忘れてた

--- components/爆破.tsx
+++ components/爆破.tsx
@@ -1,5 +1,4 @@
-import {} from "react";
-import { signIn, useSession } from "next-auth/client";
+import { useSession } from "next-auth/client";
 
 const fire = ({ profile: profile, token: token }) => {
     console.log(profile.steamid);
@@ -10,15 +9,12 @@ const fire = ({ profile: profile, token: token }) => {
         .then((body) => alert(body.message));
 };
 
-const 爆破 = (profile) => {
-    const [session, loading] = useSession();
+const 爆破 = ({ profile }) => {
+    const [session] = useSession();
     return (
         <div
             className="inline-flex items-center justify-center px-4 py-2 bg-red-500 text-red-100 border-red-600 rounded-md shadow-sm"
-            // WHY
-            onClick={() =>
-                fire({ profile: profile.profile, token: session.accessToken })
-            }
+            onClick={() => fire({ profile, token: session.accessToken })}
         >
             爆破
         </div>
