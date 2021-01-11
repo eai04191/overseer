9567d5f48efdca770ff84618a6a11dbc6093cfed
Irie Aoi <eai@mizle.net>
2021-01-11 14:43:17 +0900

    nav整理

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,12 +1,7 @@
-import Link from "next/link";
-import Image from "next/image";
-import { signIn, signOut, useSession } from "next-auth/client";
-
 export const Nav = () => {
-    const [session] = useSession();
     return (
         <nav className="relative bg-white">
-            <div className="flex items-center justify-between py-6 border-b-2 border-gray-100 space-x-10">
+            <div className="flex items-center justify-between px-4 py-6 border-b-2 border-gray-100 space-x-10">
                 <div className="flex justify-start">Overseer</div>
                 <div className="flex justify-end"></div>
             </div>
